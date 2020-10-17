9e70270c076e9ff58a4d33bd5b54da34ca991d3f
Dillon Beliveau <dillonbeliveau@gmail.com>
2020-10-17 14:20:32 -0400

    test r0 pinned to 0

--- basic.asm
+++ basic.asm
@@ -6,9 +6,33 @@ fill 1052672
 origin $00000000
 base   $80000000
 
+constant rtest_failed(30)
+
 include "lib/n64.inc"
 include "lib/header.inc"
 insert "lib/bootcode.bin"
 
 Start:
-    j Start
+    li rtest_failed, 0
+    jal test1
+      nop
+    // all passed
+    j Hang
+
+Hang:
+    j Hang
+      nop
+
+test1: // Is r0 pinned to 0?
+    addi r0, r0, 0xFF
+    lui r2, 0
+    bne r0, r2, test1_failed
+      nop
+    jr ra
+      nop
+
+
+test1_failed:
+    li rtest_failed, 1
+    j Hang
+      nop
